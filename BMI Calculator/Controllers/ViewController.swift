6ebcd5d5be9bd93a5a87f37360a9b05b93bb24af
Jay Garcia <jmrg97@gmail.com>
2023-06-14 22:36:31 -0400

    Slider Movement w/ Label Added

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        var val = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(val)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        var val = String(format: "%.f", sender.value)
+        weightLabel.text = "\(val)Kg"
+        
+    }
+    
 }
 
